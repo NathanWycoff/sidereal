dfdb721b78cd6c9378f6b2804958d86cb6918168
Nathan Wycoff <nathanbrwycoff@gmail.com>
2018-04-24 17:13:26 -0400

    roxyginzed stuff

--- R/make_plot.R
+++ R/make_plot.R
@@ -3,64 +3,69 @@
 
 ## Make the default plot for now using iPLSV
 
-library(sidereal)
 library(shiny)
 require(jsonlite)
 require(iplsv)
 
 
+#' A default test function
+#'
+#' @return Nothing, used for side effects.
+#' @export 
 #Test out the num_max function.
-set.seed(1234)
+def_func <- function() {
+    set.seed(1234)
 
-K <- 3
-V <- 4
-M <- 20
-N.mu <- 300
-P <- 2
-eta <- 2
-gamma <- 0.1 * K
-beta <- 0.1 * M
+    K <- 3
+    V <- 4
+    M <- 20
+    N.mu <- 300
+    P <- 2
+    eta <- 2
+    gamma <- 0.1 * K
+    beta <- 0.1 * M
 
-ret <- gen_plsv(K, V, M, N.mu, P, eta, gamma, beta)
+    ret <- gen_plsv(K, V, M, N.mu, P, eta, gamma, beta)
 
-runApp(list(ui = fluidPage(
-                           actionButton("do", "Update Viz"),
+    runApp(list(ui = fluidPage(
+                               actionButton("do", "Update Viz"),
 
-                           # example use of the automatically generated output function
-                           iscatrOutput("scatr1")
-                           ),
-            server = function(input, output, session) {
-                get_viz <- eventReactive(input$do, {
-                                             if (!is.null(session$userData$fit)) {
-                                                 THETA_res <- fromJSON(input$cool_id)
+                               # example use of the automatically generated output function
+                               iscatrOutput("scatr1")
+                               ),
+                server = function(input, output, session) {
+                    get_viz <- eventReactive(input$do, {
+                                                 if (!is.null(session$userData$fit)) {
+                                                     THETA_res <- fromJSON(input$cool_id)
 
-                                                 iter <- 0
-                                                 THETA_fix <- list()
-                                                 for (ti in THETA_res$title) {
-                                                     iter <- iter + 1
-                                                     ind <- as.numeric(gsub('a', '', ti))
-                                                     THETA_fix[[iter]] <- list(ind = ind, val = as.numeric(THETA_res[iter,1:2]))
+                                                     iter <- 0
+                                                     THETA_fix <- list()
+                                                     for (ti in THETA_res$title) {
+                                                         iter <- iter + 1
+                                                         ind <- as.numeric(gsub('a', '', ti))
+                                                         THETA_fix[[iter]] <- list(ind = ind, val = as.numeric(THETA_res[iter,1:2]))
+                                                     }
+                                                     print(THETA_fix)
+
+                                                     #TODO: Not a global var.
+                                                     session$userData$fit <- num_post_plsv(ret$docs, K, V, P, eta, gamma, beta,
+                                                                                           THETA_fix = THETA_fix,
+                                                                                           THETA_init = session$userData$fit$par$THETA,
+                                                                                           PSI_init = session$userData$fit$par$PSI,
+                                                                                           PHI_init = session$userData$fit$par$PHI)
+                                                 } else {
+                                                     session$userData$fit <- num_post_plsv(ret$docs, K, V, P, eta, gamma, beta)
                                                  }
-                                                 print(THETA_fix)
 
-                                                 #TODO: Not a global var.
-                                                 session$userData$fit <- num_post_plsv(ret$docs, K, V, P, eta, gamma, beta,
-                                                                                       THETA_fix = THETA_fix,
-                                                                                       THETA_init = session$userData$fit$par$THETA,
-                                                                                       PSI_init = session$userData$fit$par$PSI,
-                                                                                       PHI_init = session$userData$fit$par$PHI)
-                                             } else {
-                                                 session$userData$fit <- num_post_plsv(ret$docs, K, V, P, eta, gamma, beta)
-                                             }
+                                                 to_plot <- as.data.frame(rbind(session$userData$fit$par$THETA, session$userData$fit$par$PSI))
 
-                                             to_plot <- as.data.frame(rbind(session$userData$fit$par$THETA, session$userData$fit$par$PSI))
+                                                 return(to_plot)
+                               })
 
-                                             return(to_plot)
-                           })
-
-                # example use of the automatically generated render function
-                output$scatr1 <- renderIscatr({ 
-                    data <- get_viz()
-                    iscatr(data, col = sample(c('red', 'green'), nrow(data), replace = TRUE))
-                })
-            }))
+                    # example use of the automatically generated render function
+                    output$scatr1 <- renderIscatr({ 
+                        data <- get_viz()
+                        iscatr(data, col = sample(c('red', 'green'), nrow(data), replace = TRUE))
+                    })
+                }))
+}
